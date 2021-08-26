3d9acde5c22122644020875be74503d9b6944d89
RISHIKESH DNYANDEV SHINDE <67017632+rdshinde@users.noreply.github.com>
2021-08-26 19:38:13 +0530

    part1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./styles.css";
+import React, { useState } from "react";
 
+var emojiDictionary = {
+  "🚣‍♂️": "Person Rowing Boat",
+  "🗾": "Map of Japan",
+  "🏔": "Snow-Capped Mountain",
+  "🌋": "Volcano",
+  "🏕": "Camping",
+  "🏖": "Beach with Umbrella",
+  "🏝": "Desert Island"
+};
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We don't have this in out database.";
+    }
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>inside outt!</h1>
+      <input onChange={emojiInputHandler}></input>
+      <div>{meaning}</div>
+      {Object.keys(emojiDictionary).map((item) => {
+        return item;
+      })}
     </div>
   );
 }
